e41fbda85149be891ef480371f9163b596ba18ba
SanskritiGupta19 <19tanu.gupta@gmail.com>
2023-07-28 22:49:23 -0700

    first commit

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyBo6LneRsFMO2Ejs3H5OuJuZpcjM_zMqgU",
+    authDomain: "content-management-syste-7356b.firebaseapp.com",
+    projectId: "content-management-syste-7356b",
+    storageBucket: "content-management-syste-7356b.appspot.com",
+    messagingSenderId: "559682698402",
+    appId: "1:559682698402:web:de7d4e07e1126aea7f6bfb"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
